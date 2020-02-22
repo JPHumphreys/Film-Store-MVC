cdc844a926877507cecf25249fde2cf59a073dc7
JPHumphreys <Jonathan.Humphreys@academytrainee.com>
2020-02-22 16:10:35 +0000

    added some data and output caching

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -3,6 +3,8 @@ using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
 using Vidly.ViewModels;
+using System.Runtime.Caching;
+using System.Collections.Generic;
 
 namespace Vidly.Controllers
 {
@@ -64,6 +66,13 @@ namespace Vidly.Controllers
         // GET: Customers
         public ViewResult Index()
         {
+            if(MemoryCache.Default["Genres"] == null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+
             return View();
         }
 
